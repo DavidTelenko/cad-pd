6ea6a1bec13366c1de0681deae2650c45916bf22
David Telen'ko <davidtelenko11@gmail.com>
2021-11-25 02:48:32 +0200

    Co-authored-by: Dmytro Sushchenko <OBess@users.noreply.github.com>

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import nprogress from "nprogress";
 import InputGroup from "../components/InputGroup.vue";
 import Modeler from "../components/Modeler.vue";
 import NotFoundComponent from "../components/NotFoundComponent.vue";
-import Test from "../components/Test.vue";
 
 const routes = [
     {
@@ -19,11 +18,6 @@ const routes = [
             if (from.name == "input") next();
             else next('/');
         },
-    },
-    {
-        path: "/test",
-        name: "test",
-        component: Test,
     },
     {
         path: '/:pathMatch(.*)*',
