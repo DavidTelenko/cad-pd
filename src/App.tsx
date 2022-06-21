20233c8e52aa856ccd164424c4beaa37c684132b
Anastasiia Yedyharova <yedyharova.nastya@gmail.com>
2022-06-21 21:01:00 +0200

    add file upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, ChangeEvent } from "react";
 import "./App.css";
 import "./Styles/FileUploader.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -7,11 +7,30 @@ import Modeler from "./Components/Modeler";
 import NotFound from "./Components/NotFound";
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState<File | undefined>();
+
+  const changeHandler = (event: ChangeEvent) => {
+    const target = event.target as HTMLInputElement;
+    if (target.files) {
+      setSelectedFile(target.files[0]);
+    }
+  };
+
+  useEffect(() => console.log(selectedFile), [selectedFile]);
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<FileUploader />} />
+          <Route
+            path="/"
+            element={
+              <FileUploader
+                selectedFile={selectedFile}
+                changeHandler={changeHandler}
+              />
+            }
+          />
           <Route path="/modeler" element={<Modeler />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
--- src/Components/FileUploader.tsx
+++ src/Components/FileUploader.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FC, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Slider from "@material-ui/core/Slider";
 import { Button } from "@material-ui/core";
@@ -8,66 +8,78 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 import FormControl from "@material-ui/core/FormControl";
 import { Link } from "react-router-dom";
 // import AddLinkIcon from "@mui/icons-material/AddLink";
+// import FileUploadIcon from "@mui/icons-material/FileUpload";
 
 const useStyles = makeStyles({
   root: {
-    width: 550,
+    width: "90%",
   },
   margin: {
-    // margin: theme.spacing(1),
+    margin: "5px",
   },
 });
 
-const valuetext = (value: number): string => {
-  return `${value}°C`;
+type FileUploaderProps = {
+  selectedFile: File | undefined;
+  changeHandler: (event: ChangeEvent) => void;
 };
 
-const FileUploader: FC = () => {
+const FileUploader = ({ selectedFile, changeHandler }: FileUploaderProps) => {
   const classes = useStyles();
 
-  const [selectedFile, setSelectedFile] = useState<File>();
   const [value, setValue] = useState<number | number[]>(2.5);
 
-  const changeHandler = (event: ChangeEvent) => {
-    const target = event.target as HTMLInputElement;
-    if (target.files) setSelectedFile(target.files[0]);
-    console.log(selectedFile);
-  };
-
   const onSliderChange = (value: number | number[]) => {
-    // const target = e.target as HTMLInputElement;
     setValue(value);
   };
-
-  const handleSubmission = () => {};
 
   return (
     <>
       <div className="FileUploader">
-        <input
-          type="file"
-          name="file"
-          onChange={(e) => changeHandler(e)}
-          accept=".gltf,.glb"
-        />
-        {selectedFile ? (
+        {/* <>
+          <input
+            type="file"
+            name="file"
+            onChange={(e) => changeHandler(e)}
+            accept=".gltf,.glb"
+          />
+          {selectedFile ? (
+            <div>
+              <p>Filename: {selectedFile.name}</p>
+              <p>Filetype: {selectedFile.type}</p>
+              <p>Size in bytes: {selectedFile.size}</p>
+              <p>
+                lastModifiedDate: {selectedFile.lastModified.toLocaleString()}
+              </p>
+            </div>
+          ) : (
+            <p>Select a file to show details</p>
+          )}
           <div>
-            <p>Filename: {selectedFile.name}</p>
-            <p>Filetype: {selectedFile.type}</p>
-            <p>Size in bytes: {selectedFile.size}</p>
-            <p>
-              lastModifiedDate: {selectedFile.lastModified.toLocaleString()}
-            </p>
+            <button onClick={handleSubmission}>Submit</button>
           </div>
-        ) : (
-          <p>Select a file to show details</p>
-        )}
-        <div>
-          <button onClick={handleSubmission}>Submit</button>
-        </div>
+        </> */}
         <FormControl className={classes.margin}>
           <InputLabel htmlFor="input-with-icon-adornment">
-            With a start adornment
+            Upload GLTF/GLB file
+          </InputLabel>
+          <Input
+            id="input-with-icon-adornment"
+            type="file"
+            name="file"
+            onChange={(event) => changeHandler(event)}
+            startAdornment={
+              <InputAdornment position="start">
+                {/* <FileUploadIcon /> */}
+              </InputAdornment>
+            }
+            placeholder="No file was chosen"
+            inputProps={{ accept: ".gltf,.glb", readOnly: true }}
+          />
+        </FormControl>
+        <FormControl className={classes.margin}>
+          <InputLabel htmlFor="input-with-icon-adornment">
+            Enter valid GLTF/GLB link
           </InputLabel>
           <Input
             id="input-with-icon-adornment"
@@ -76,41 +88,24 @@ const FileUploader: FC = () => {
                 {/* <AddLinkIcon /> */}
               </InputAdornment>
             }
+            inputProps={{
+              pattern: ".+.gltf|.+.glb",
+            }}
           />
         </FormControl>
-        <div className="form-group py-3" v-if="!$store.sharedModelFile">
-          <label className="form-label">
-            <i className="fas fa-link"></i> GLTF/GLB Link
-          </label>
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="basic-addon1">
-                .gltf
-              </span>
-            </div>
-            <input
-              className="form-control input-group"
-              v-model="$store.sharedModelLink"
-              placeholder="Link"
-              pattern=".+\.gltf|.+\.glb"
-              type="input"
-            />
-          </div>
-        </div>
         <div className={classes.root}>
           <div>{`Scale ${value}`}</div>
           <Slider
             defaultValue={2.5}
-            getAriaValueText={valuetext}
             aria-labelledby="discrete-slider-small-steps"
             step={0.05}
             min={0.05}
             max={4.95}
             valueLabelDisplay="auto"
-            onChange={(event, value) => onSliderChange(value)}
+            onChange={(_event, value) => onSliderChange(value)}
           />
         </div>
-        <Link to="/modeler">
+        <Link to="/modeler" onClick={() => console.log(selectedFile)}>
           <Button variant="contained" color="primary">
             Continue
           </Button>
