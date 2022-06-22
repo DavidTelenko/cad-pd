30e9df5bcbcee75d0e5480b92a20db04203a1c81
Anastasiia Yedyharova <yedyharova.nastya@gmail.com>
2022-06-22 14:21:57 +0200

    react: lib test

--- src/Components/Modeler.tsx
+++ src/Components/Modeler.tsx
@@ -1,13 +1,63 @@
-import React, { FC } from "react";
-import Webcam from "react-webcam";
+// import { Canvas, useFrame } from "react-three-fiber";
+// import { Stats, OrbitControls } from "@react-three/drei";
+// // import * as three from "three";
+import React, { Suspense, useState } from "react";
+import { DefaultXRControllers, ARCanvas, Interactive } from "@react-three/xr";
+// import { Text } from "@react-three/drei";
 
-const Modeler: FC = () => {
+function Box({ color, size, scale, children, ...rest }: any) {
   return (
-    <>
-      <div>Modeler</div>
-      <Webcam />
-    </>
+    <mesh scale={scale} {...rest}>
+      <boxBufferGeometry attach="geometry" args={size} />
+      <meshPhongMaterial attach="material" color={color} />
+      {children}
+    </mesh>
   );
-};
+}
 
-export default Modeler;
+function Button(props: any) {
+  const [hover, setHover] = useState(false);
+  const [color, setColor] = useState<any>("blue");
+
+  const onSelect = () => {
+    setColor((Math.random() * 0xffffff) | 0);
+  };
+
+  return (
+    <Interactive
+      onHover={() => setHover(true)}
+      onBlur={() => setHover(false)}
+      onSelect={onSelect}
+    >
+      <Box
+        color={color}
+        scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]}
+        size={[0.4, 0.1, 0.1]}
+        {...props}
+      >
+        <Suspense fallback={null}>
+          {/* <Text
+            position={[0, 0, 0.06]}
+            fontSize={0.05}
+            color="#000"
+            anchorX="center"
+            anchorY="middle"
+          >
+            Hello react-xr!
+          </Text> */}
+        </Suspense>
+      </Box>
+    </Interactive>
+  );
+}
+
+export default function Modeler() {
+  return (
+    <ARCanvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Button position={[0, 0.1, -0.2]} />
+      <DefaultXRControllers />
+    </ARCanvas>
+  );
+}
