8b8251dc9fde135554325cbdec9d42e44668aeed
Anastasiia Yedyharova <yedyharova.nastya@gmail.com>
2022-06-23 09:11:36 +0200

    react-add: video

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
     const target = event.target as HTMLInputElement;
     if (target.files) {
       setSelectedFile(target.files[0]);
+      console.log(target.value);
     }
   };
 
--- src/Components/Modeler.jsx
+++ src/Components/Modeler.jsx
@@ -2,7 +2,8 @@
 // import { Stats, OrbitControls } from "@react-three/drei";
 // // import * as three from "three";
 import React, { Suspense, useState, useRef } from "react";
-import { DefaultXRControllers, ARCanvas, Interactive } from "@react-three/xr";
+import { ARCanvas, ARMarker } from "@artcom/react-three-arjs";
+import { DefaultXRControllers, Interactive } from "@react-three/xr";
 import { Text, useGLTF } from "@react-three/drei";
 import { useLoader, useFrame } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
@@ -13,7 +14,7 @@ import Model from "./Model";
 //   link: string,
 // };
 
-function Box({ color, size, scale, children, ...rest }) {
+function Box({ size, scale, children, ...rest }) {
   const boxRef = useRef();
 
   useFrame(() => {
@@ -30,43 +31,39 @@ function Box({ color, size, scale, children, ...rest }) {
     >
       {/* <textGeometry args={["test", { font, size: 5, height: 1 }]} /> */}
       <boxBufferGeometry attach="geometry" args={size} />
-      <meshPhongMaterial attach="material" color={color} />
+      <meshPhongMaterial attach="material" />
       {children}
     </mesh>
   );
 }
 
-function Button(props) {
-  const [hover, setHover] = useState(false);
-  const [color, setColor] = useState("blue");
+// function Box() {
+//   return (
+//     <mesh>
+//       <boxBufferGeometry args={[1, 1, 1]} />
+//       <meshStandardMaterial color={"hotpink"} />
+//     </mesh>
+//   );
+// }
 
-  const onSelect = () => {
-    setColor((Math.random() * 0xffffff) | 0);
-  };
-
+function Target(props) {
   const gltf = useLoader(
     GLTFLoader,
     "https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
   );
 
   return (
-    <Interactive
-      onHover={() => setHover(true)}
-      onBlur={() => setHover(false)}
-      onSelect={onSelect}
+    <Box
+      // scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]}
+      scale={0.007} //{[0.08, 0.08, 0.08]}
+      size={[0.03, 0.03, 0.03]}
+      {...props}
     >
-      <Box
-        color={color}
-        // scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]}
-        scale={0.07} //{[0.08, 0.08, 0.08]}
-        size={[0.03, 0.03, 0.03]}
-        {...props}
-      >
-        <Suspense fallback={null}>
-          <primitive object={gltf.scene} />
+      <Suspense fallback={null}>
+        <primitive object={gltf.scene} />
 
-          {/* <Model /> */}
-          {/* <Text
+        {/* <Model /> */}
+        {/* <Text
             position={[0, 0, 0.06]}
             fontSize={0.05}
             color="#000"
@@ -75,20 +72,55 @@ function Button(props) {
           >
             Hello react-xr!
           </Text> */}
-        </Suspense>
-      </Box>
-    </Interactive>
+      </Suspense>
+    </Box>
   );
 }
 
 export default function Modeler({ link }) {
   return (
-    <ARCanvas>
+    // <ARCanvas>
+    //   <ambientLight />
+    //   <pointLight position={[10, 10, 10]} />
+    //   <Button position={[0, 0.5, -0.2]} />
+    //   <Button position={[0, -0.5, 0.2]} />
+    //   <DefaultXRControllers />
+    // </ARCanvas>
+    // <a-scene embedded arjs>
+    //   <a-marker id="asset" preset="hiro">
+    //     <a-box
+    //       position="0 0 0"
+    //       scale={1.0}
+    //       gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
+    //     ></a-box>
+    //   </a-marker>
+    //   <a-entity camera></a-entity>
+    // </a-scene>
+    <ARCanvas
+      gl={{
+        antialias: false,
+        powerPreference: "default",
+        physicallyCorrectLights: true,
+      }}
+      onCameraStreamReady={() => console.log("Camera stream ready")}
+      onCameraStreamError={() => console.error("Camera stream error")}
+      onCreated={({ gl }) => {
+        gl.setSize(window.innerWidth, window.innerHeight);
+      }}
+    >
       <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Button position={[0, 0.5, -0.2]} />
-      <Button position={[0, -0.5, 0.2]} />
-      <DefaultXRControllers />
+      <pointLight position={[10, 10, 0]} intensity={10.0} />
+      <ARMarker
+        params={{ smooth: true }}
+        type={"pattern"}
+        patternUrl={"../public/patt.hiro"}
+        onMarkerFound={() => {
+          console.log("Marker Found");
+        }}
+      >
+        {/* <Box /> */}
+        <Target position={[0, -0.5, 0.2]} />
+      </ARMarker>
     </ARCanvas>
   );
 }
