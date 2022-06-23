a782d6c6e4caa6ead2ecd0b3476df0f29c3ce3e5
David Telen'ko <davidtelenko11@gmail.com>
2022-06-23 12:46:23 +0300

    latest

--- src/Components/Modeler.jsx
+++ src/Components/Modeler.jsx
@@ -1,14 +1,15 @@
 // import { Canvas, useFrame } from "react-three-fiber";
 // import { Stats, OrbitControls } from "@react-three/drei";
 // // import * as three from "three";
-import React, { Suspense, useState, useRef } from "react";
-import { ARCanvas, ARMarker } from "@artcom/react-three-arjs";
-import { DefaultXRControllers, Interactive } from "@react-three/xr";
-import { Text, useGLTF } from "@react-three/drei";
-import { useLoader, useFrame } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import { DefaultXRControllers, Interactive } from "@react-three/xr";
+// import { Text, useGLTF } from "@react-three/drei";
 // import { extend } from "react-three-fiber";
-import Model from "./Model";
+// import Model from "./Model";
+// import React, { Suspense, useState, useRef } from "react";
+// import { useLoader, useFrame } from "@react-three/fiber";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+import { ARCanvas, ARMarker } from "@artcom/react-three-arjs";
 
 // <ARCanvas>
 //   <ambientLight />
@@ -55,57 +56,46 @@ import Model from "./Model";
 //   );
 // }
 
-function Box() {
-  return (
-    <mesh>
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={"hotpink"} />
-    </mesh>
-  );
-}
+// function Box() {
+//   return (
+//     <mesh>
+//       <boxBufferGeometry args={[1, 1, 1]} />
+//       <meshStandardMaterial color={"hotpink"} />
+//     </mesh>
+//   );
+// }
 
-function Target(props) {
-  const gltf = useLoader(
-    GLTFLoader,
-    "https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
-  );
+// function Target(props) {
+//   const gltf = useLoader(
+//     GLTFLoader,
+//     "https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
+//   );
 
-  return (
-    <Box
-      // scale={0.007} //{[0.08, 0.08, 0.08]}
-      size={[0.03, 0.03, 0.03]}
-      {...props}
-    >
-      <Suspense fallback={null}>
-        <primitive object={gltf.scene} />
-      </Suspense>
-    </Box>
-  );
-}
+//   return (
+//     <Box
+//       // scale={0.007} //{[0.08, 0.08, 0.08]}
+//       // size={[0.03, 0.03, 0.03]}
+//       {...props}
+//     >
+//       <Suspense fallback={null}>
+//         <primitive object={gltf.scene} />
+//       </Suspense>
+//     </Box>
+//   );
+// }
 
 export default function Modeler({ link }) {
   return (
     <ARCanvas
       camera={{ position: [0, 0, 0] }}
-      gl={{
-        antialias: false,
-        powerPreference: "default",
-        physicallyCorrectLights: true,
-      }}
+      dpr={window.devicePixelRatio}
       onCreated={({ gl }) => {
         gl.setSize(window.innerWidth, window.innerHeight);
       }}
     >
       <ambientLight />
-      <pointLight position={[10, 10, 0]} intensity={10.0} />
-      <ARMarker
-        params={{ smooth: true }}
-        type={"pattern"}
-        patternUrl={"../public/hiro.patt"}
-        onMarkerFound={() => {
-          console.log("Marker Found");
-        }}
-      >
+      <pointLight position={[10, 10, 0]} />
+      <ARMarker type={"pattern"} patternUrl={"data/hiro.patt"}>
         <mesh>
           <boxBufferGeometry args={[1, 1, 1]} />
           <meshStandardMaterial color={"green"} />
