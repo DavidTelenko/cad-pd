f79e60e1c307d53b7d5ddf205919cad40191930f
yedyharova.nastya <yedyharova.nastya@gmail.com>
2022-08-11 23:31:33 +0200

    feat: add cube model to hiro marker

---
+++ src/Components/CadAr.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { ARCanvas, ARMarker } from "@artcom/react-three-arjs";
+
+import "../Styles/CadAr.css";
+
+const CadAr = () => {
+    return (<ARCanvas
+        className="canvas"
+        camera={{ position: [0, 0, 0] }}
+        dpr={window.devicePixelRatio}
+        onCreated={({ gl }) => {
+          gl.setSize(window.innerWidth, window.innerHeight);
+        }}
+      >
+        <ambientLight />
+        <pointLight position={[10, 10, 0]} />
+        <ARMarker type={"pattern"} patternUrl={"data/hiro.patt"}>
+          <mesh>
+            <boxBufferGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={"green"} />
+          </mesh>
+        </ARMarker>
+      </ARCanvas>);
+}
+
+export default CadAr;
